f6a0d907faa2b30b5a0fa9069459bbd2f0a7e947
Matthias Karl <itgkarl+git@gmail.com>
2015-04-04 07:39:41 -0700

    Basisprojekt angelegt

---
+++ SeleniumPoFramework/src/main/java/de/muenchen/ha3/a23/SeleniumPoFramework/App.java
@@ -0,0 +1,13 @@
+package de.muenchen.ha3.a23.SeleniumPoFramework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
