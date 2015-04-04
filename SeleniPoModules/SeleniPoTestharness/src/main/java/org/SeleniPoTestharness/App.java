8ea058d6c9c3ca5b346b5124bf9e5ec0ff3ee4db
Matthias Karl <itgkarl+git@gmail.com>
2015-04-04 11:03:41 -0700

    Multimodule Projekt angelegt

---
+++ SeleniPoModules/SeleniPoTestharness/src/main/java/org/SeleniPoTestharness/App.java
@@ -0,0 +1,21 @@
+package org.SeleniPoTestharness;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	Logger log = Logger.getLogger(App.class);
+	
+	public void performLo() {
+		log.info("HelloWorld");
+
+	}
+
+	public static void main(String[] args) {
+		App a = new App();
+		a.performLo();
+	}
+}
